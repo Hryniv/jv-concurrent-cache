efaf03fe6459df1034f84a6046687b5070f1ae05
Yurii <wervmer@gmail.com>
2025-11-30 09:43:06 +0200

    added implementation of Cache class

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,61 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private Map<K, V> cache = new HashMap<>();
+
     public V get(K key) {
-        return null;
+        V value = null;
+        try {
+            readWriteLock.readLock().lock();
+            value = cache.get(key);
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
+
+        return value;
     }
 
     public void put(K key, V value) {
-
+        try {
+            readWriteLock.writeLock().lock();
+            cache.put(key, value);
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 
     public void remove(K key) {
-
+        try {
+            readWriteLock.writeLock().lock();
+            cache.remove(key);
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 
     public void clear() {
-
+        try {
+            readWriteLock.writeLock().lock();
+            cache.clear();
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        int size = 0;
+        try {
+            readWriteLock.readLock().lock();
+            size = cache.size();
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
+        return size;
     }
 }
